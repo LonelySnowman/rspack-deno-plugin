9c9bb9d04ae2efb1ec96ec38ef977dde54bbfecc
LonelySnowman <2438490168@qq.com>
2025-02-16 21:10:24 +0800

    feat: add npm version package

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    clean: true,
+    splitting: true,
+    format: ['cjs', 'esm'],
+    outDir: 'lib',
+    dts: true,
+  },
+]);
